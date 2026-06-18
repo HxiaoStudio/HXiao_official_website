2bcacf1c7baf974c60ad3d4b71e3c81b992d79e9
青山公仔 <qingshangongzai@163.com>
2026-06-18 15:17:27 +0800

    [配置] 站点域名更新为 hxiaostudio.cn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://hxiao.studio',
+  site: 'https://hxiaostudio.cn',
 });
